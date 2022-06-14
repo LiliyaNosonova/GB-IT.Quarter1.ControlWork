dadd3ba736a079d14bbad9dd3e4f064cbf1cc74f
LNosonova <l.nosonova@icloud.com>
2022-06-14 18:38:10 +0300

    Add Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача. Написать программу, 
+// которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше или равна 3 символа.
+
+string[] array = { "Hello", "2", "world", ":-)" };
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length < 4)
+    {
+        count++;
+    }
+}
+
+string[] result = new string[count];
+count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length < 4)
+    {
+        result[count] = array[i];
+        count++;
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        System.Console.WriteLine($"{j} элемент искомого массива: {array[j]}");
+    }
+}
+PrintArray(result);
+
